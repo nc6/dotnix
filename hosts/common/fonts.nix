d529434a0d9cc28ad93a138aa44ef14d8072e9f0
Nicholas Clarke <nick@topos.org.uk>
2022-04-11 10:36:43 +0200

    Switch to using i3status-rs for status bar.

--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -6,6 +6,7 @@
     fira-code
     fira-code-symbols
     fira-mono
+    font-awesome_5
     emacs-all-the-icons-fonts
   ];
 }
--- modules/xorg.nix
+++ modules/xorg.nix
@@ -1,4 +1,54 @@
 { pkgs, lib, ...}:{
+
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      top = {
+        blocks = [
+          {
+            block = "disk_space";
+            path = "/";
+            alias = "/";
+            info_type = "available";
+            unit = "GB";
+            interval = 60;
+            warning = 20.0;
+            alert = 10.0;
+          }
+          {
+            block = "memory";
+            display_type = "memory";
+            format_mem = "{mem_used_percents}";
+            format_swap = "{swap_used_percents}";
+          }
+          {
+            block = "cpu";
+            interval = 1;
+          }
+          {
+            block = "load";
+            interval = 1;
+            format = "{1m}";
+          }
+          { block = "networkmanager"; }
+          { block = "sound"; }
+          { block = "keyboard_layout";
+            driver = "localebus";
+          }
+          {
+            block = "time";
+            interval = 60;
+            format = "%a %F %R";
+          }
+        ];
+        settings = {
+          theme = "solarized-dark";
+        };
+        icons = "awesome5";
+      };
+    };
+  };
+
   services.betterlockscreen = {
     enable = true;
   };
@@ -39,7 +89,8 @@
 
       bars = [
         { position = "top";
-          statusCommand = "${pkgs.i3status}/bin/i3status";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
+          trayOutput = "primary";
         }
       ];
     };
