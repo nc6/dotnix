a71739d52dd3180e7228a7d84bdb3b9ffb763fa3
Nicholas Clarke <nick@topos.org.uk>
2022-05-23 17:53:35 +0200

    Switch to using cache.iog.io
    This should be faster than using hydra directly.

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -7,11 +7,11 @@
     settings = {
       substituters = [
         "https://cache.nixos.org"
-        "https://hydra.iohk.io"
+        "https://cache.iog.io"
       ];
 
       trusted-public-keys = [
-        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        "cache.iog.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
     };
   };
