a70f90d1aec43d148f23a669da3655c30726d639
Nicholas Clarke <nick@topos.org.uk>
2022-10-03 12:17:29 +0200

    Update udev rules for the Moonlander.

--- hosts/common/udev.nix
+++ hosts/common/udev.nix
@@ -3,8 +3,15 @@ let oryxRules =
   pkgs.writeTextFile {
     name = "50-oryx.rules";
     text = ''
-      # Rule for the Moonlander
+      # Rules for Oryx web flashing and live training
+      KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
+      KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+
+      # Legacy rules for live training over webusb (Not needed for firmware v21+)
       SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="plugdev"
+
+      # Wally Flashing rules for the Moonlander and Planck EZ
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu"
     '';
     destination = "/etc/udev/rules.d/50-oryx.rules";
   };
