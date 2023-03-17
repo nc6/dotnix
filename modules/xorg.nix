39457e3bea880c5167aa675bf2d3af9c3da33009
Nicholas Clarke <nick@topos.org.uk>
2023-03-17 11:53:20 +0100

    fixup! Update i3status-rust config.

--- modules/xorg.nix
+++ modules/xorg.nix
@@ -9,16 +9,13 @@
             block = "disk_space";
             path = "/";
             info_type = "available";
-            unit = "GB";
+            alert_unit = "GB";
             interval = 60;
             warning = 20.0;
             alert = 10.0;
           }
           {
             block = "memory";
-            display_type = "memory";
-            format_mem = "{mem_used_percents}";
-            format_swap = "{swap_used_percents}";
           }
           {
             block = "cpu";
@@ -26,16 +23,14 @@
           }
           {
             block = "load";
-            interval = 1;
-            format = "{1m}";
           }
-          { block = "networkmanager"; }
+          { block = "net"; }
           { block = "sound"; }
           {
             block = "battery";
             interval = 10;
-            format = "{percentage} {time}";
-            hide_missing = true;
+            format = "$icon $percentage $time";
+            missing_format = "";
           }
           { block = "keyboard_layout";
             driver = "localebus";
@@ -43,11 +38,13 @@
           {
             block = "time";
             interval = 60;
-            format = "%a %F %R";
+            format = "$timestamp.datetime(f:'%a %F %R')";
           }
         ];
         settings = {
-          theme = "solarized-dark";
+          theme = {
+            theme = "solarized-dark";
+          };
         };
         icons = "awesome5";
       };
