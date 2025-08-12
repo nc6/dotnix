f4ba3e74a890d075d6345ac5e49d6de056429724
Nicholas Clarke <nick@topos.org.uk>
2025-08-12 13:51:21 +0200

    Update jujutsu config

--- modules/jujutsu.nix
+++ modules/jujutsu.nix
@@ -5,8 +5,24 @@
     settings = {
       user = {
         name = "Nicholas Clarke";
-        email = "nc@topos.org.uk";
+        email = "nick@topos.org.uk";
       };
+
+      signing = {
+        behaviour = "drop";
+        backend = "ssh";
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHpLeHabhvyaHKr0EXrchPir8yqX5UM+H6ZfaB9nx1Q";
+        backends = {
+          ssh = {
+            program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+          };
+        };
+      };
+
+      git = {
+        sign-on-push = true;
+      };
+
     };
   };
 }
