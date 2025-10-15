f5b4a7f222740be533d18b759671ca7021746059
Nicholas Clarke <nick@topos.org.uk>
2025-10-15 18:52:43 +0200

    Fix greetd config

--- hosts/common/greeter.nix
+++ hosts/common/greeter.nix
@@ -2,11 +2,11 @@
 {
   services.greetd = {
     enable = true;
-    # settings = {
-    #   default_session = {
-    #     user = "greeter";
-    #     command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${pkgs.sway}/bin/sway";
-    #   };
-    # };
+    settings = {
+      default_session = {
+        user = "greeter";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time";
+      };
+    };
   };
 }
