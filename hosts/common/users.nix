18036a1fd1c07b9f2a9732411d3d718d458823eb
Nicholas Clarke <nick@topos.org.uk>
2026-02-16 08:58:25 +0100

    Add authorized_keys to user nc

--- hosts/common/users.nix
+++ hosts/common/users.nix
@@ -5,5 +5,9 @@
     isNormalUser = true;
     uid = 1000;
     extraGroups = ["input" "plugdev" "wheel" "networkmanager" "libvirtd"];
+    openssh.authorizedKeys.keys =
+    [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHpLeHabhvyaHKr0EXrchPir8yqX5UM+H6ZfaB9nx1Q"
+    ];
   };
 }
